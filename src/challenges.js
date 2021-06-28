dde1277954ff80ff216a9bff3f0e83df86d38741
Pedro Delicoli <pedrodelicoli@hotmail.com>
2021-06-28 14:05:47 -0300

    Projeto Playground Functions

---
+++ src/challenges.js
@@ -0,0 +1,118 @@
+// Desafio 1
+function compareTrue(value1, value2) {
+  if (value1 === true && value2 === true) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+// Desafio 2
+function calcArea(base, height) {
+  let sum = (base * height) / 2;
+  return sum;
+}
+
+// Desafio 3
+function splitSentence(stringtosplit) {
+  let splits = stringtosplit.split(' ');
+  return splits;
+}
+
+// Desafio 4
+function concatName(string) {
+  let orderString = string[string.length - 1] + ', ' + string[0];
+  return orderString;
+}
+
+// Desafio 5
+function footballPoints(wins, ties) {
+  let winspoints = 3 * wins;
+  let tiespoints = 1 * ties;
+  let pontos = winspoints + tiespoints;
+  return pontos;
+}
+
+// Desafio 6
+function highestCount(numeros) {
+  let maior = numeros[0];
+  let contNumero = 0;
+  for (let index = 0; index < numeros.length; index += 1) {
+    if (numeros[index] > maior) {
+      maior = numeros[index];
+    }
+  }
+  for (let index2 = 0; index2 < numeros.length; index2 += 1) {
+    if (numeros[index2] === maior) {
+      contNumero += 1;
+    }
+  }
+  return contNumero;
+}
+
+// Desafio 7
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1position = Math.abs(cat1 - mouse);
+  let cat2position = Math.abs(cat2 - mouse);
+
+  if (cat1position < cat2position) {
+    return 'cat1';
+  }
+  else if (cat1position === cat2position) {
+    return 'os gatos trombam e o rato foge';
+  }
+  else {
+    return 'cat2';
+  }
+}
+
+// Desafio 8
+function fizzBuzz(string) {
+  for (let index in string) {
+    if (string[index] % 5 === 0 && string[index] % 3 === 0) {
+      string[index] = 'fizzBuzz';
+    }
+    else if (string[index] % 5 === 0) {
+      string[index] = 'buzz';
+    }
+    else if (string[index] % 3 === 0) {
+      string[index] = 'fizz';
+    }
+    else {
+      string[index] = 'bug!';
+    }
+  }
+  return string;
+}
+
+// Desafio 9
+function encode(stringcod) {
+  stringcod = stringcod.replace(/a/g, '1');
+  stringcod = stringcod.replace(/e/g, '2');
+  stringcod = stringcod.replace(/i/g, '3');
+  stringcod = stringcod.replace(/o/g, '4');
+  stringcod = stringcod.replace(/u/g, '5');
+  return stringcod;
+}
+function decode(stringcod) {
+  stringcod = stringcod.replace(/1/g, 'a');
+  stringcod = stringcod.replace(/2/g, 'e');
+  stringcod = stringcod.replace(/3/g, 'i');
+  stringcod = stringcod.replace(/4/g, 'o');
+  stringcod = stringcod.replace(/5/g, 'u');
+  return stringcod; 
+}
+
+module.exports = {
+  calcArea,
+  catAndMouse,
+  compareTrue,
+  concatName,
+  decode,
+  encode,
+  fizzBuzz,
+  footballPoints,
+  highestCount,
+  splitSentence,
+};
---
+++ src/challenges2.js
@@ -0,0 +1,100 @@
+// Desafio 10
+function techList(techs, nome) {
+  let techsListAll = techs.sort();
+  let techList1 = {
+    tech: techsListAll[0],
+    name: nome,
+  };
+  let techList2 = {
+    tech: techsListAll[1],
+    name: nome,
+  };
+  let techList3 = {
+    tech: techsListAll[2],
+    name: nome,
+  };
+  let techList4 = {
+    tech: techsListAll[3],
+    name: nome,
+  };
+  let techList5 = {
+    tech: techsListAll[4],
+    name: nome,
+  };
+  if (techsListAll.length === 0) {
+    return 'Vazio!';
+  }
+  return [techList1, techList2, techList3, techList4, techList5];
+}
+
+// Desafio 11
+function generatePhoneNumber(numeros) {
+  if (numeros.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let index in numeros) {
+    if (numeros[index] < 0 || numeros[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let numeroRepetido = 0;
+  for (let index2 in numeros) {
+    let contNumero = 0;
+    numeroRepetido = numeros[index2];
+    for (let index3 in numeros) {
+      if (numeros[index3] === numeroRepetido) {
+        contNumero += 1;
+      }
+    }
+    if (contNumero >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  let mask = '(##) #####-####';
+  let novoNum = '';
+  let posicao = 0;
+  for (let i = 0; i < mask.length; i += 1) {
+    if (mask[i] === '#') {
+      novoNum += numeros[posicao];
+      posicao += 1;
+    }
+    else {
+      novoNum += mask[i];
+    }
+  }
+  return novoNum;
+}
+
+// Desafio 12
+function triangleCheck(lineA, lineB, lineC) {
+  if (lineA > (lineB + lineC) || lineB > (lineA + lineC) || lineC > (lineB + lineA)) {
+    return false;
+  }
+  else if (lineA < Math.abs(lineB - lineC) || lineB < Math.abs(lineA - lineC) || lineC < Math.abs(lineA - lineB)) {
+    return false;
+  }
+  return true;
+}
+console.log(triangleCheck(10, 14, 8));
+
+// Desafio 13
+// https://codereview.stackexchange.com/questions/115885/extract-numbers-from-a-string-javascript
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+
+function hydrate(drinks) {
+  let numbers = drinks.match(/\d+/g).map(Number);
+  let cupsTotal = numbers.reduce((accum, curr) => accum + curr);
+  let unity = 'copos';
+  if (cupsTotal === 1) {
+    unity = 'copo';
+  };
+
+  return cupsTotal + ' ' + unity + ' ' + 'de água';
+}
+
+module.exports = {
+  generatePhoneNumber,
+  techList,
+  hydrate,
+  triangleCheck,
+};
